ec2665b5f7a2d35db0fd9863cd4e232133e4e438
Adrian Cruceru <cruceruadrian@gmail.com>
2020-06-09 12:35:21 +0200

    Update format function

--- src/types.rs
+++ src/types.rs
@@ -11,7 +11,6 @@ use std::borrow::Cow;
 use std::str;
 use std::fmt;
 use chrono::{self, Utc, Datelike, Timelike, TimeZone};
-use std::fmt::Write;
 
 use {DerWrite, oid};
 
@@ -75,37 +74,32 @@ impl Name {
     }
 }
 
-fn format_oid(oid: &ObjectIdentifier) -> String {
-    match oid::OID_TO_NAME.get(oid) {
-        Some(o) => o.to_string(),
-        None => oid.components().iter().map(|c| c.to_string()).collect::<Vec<_>>().join(".")
-    }
-}
-
-fn format_der(der: &TaggedDerValue) -> String {
-    if der.pcbit() != PCBit::Constructed {
-        match der.tag() {
-            TAG_NUMERICSTRING | TAG_PRINTABLESTRING | TAG_IA5STRING | TAG_UTF8STRING => {
-                return String::from_utf8_lossy(&der.value()).to_string();
-            }
-            _ => {} // Fall through to hex print
-        }
-    }
-    
-    let mut s = String::new();
-    for &byte in der.value() {
-        write!(&mut s, "{:x}", byte).expect("Unable to write");
-    }
-    s
-}
-
-fn format_rdr(rdr: &(ObjectIdentifier, TaggedDerValue)) -> String {
-    format!("{}={}", format_oid(&rdr.0), format_der(&rdr.1))
-}
-
 impl fmt::Display for Name {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.value.iter().map(|rdr| format_rdr(rdr)).collect::<Vec<_>>().join(", "))?;
+        for (p,v) in self.value.iter().enumerate() {
+            if p > 0 {
+                write!(f, ", ")?;
+            }
+
+            if let Some(o) = oid::OID_TO_NAME.get(&v.0) {
+                write!(f, "{}", o)?;
+            } else {
+                for (p1,v1) in v.0.components().iter().enumerate() {
+                    if p1 > 0 {
+                        write!(f, ".")?;
+                    }
+                    write!(f, "{}", v1)?;
+                }
+            }
+            write!(f, "=")?;
+            match (v.1.pcbit(), v.1.tag()) {
+                (PCBit::Primitive, TAG_NUMERICSTRING) | (PCBit::Primitive, TAG_PRINTABLESTRING) | (PCBit::Primitive, TAG_IA5STRING) | (PCBit::Primitive, TAG_UTF8STRING) =>
+                    write!(f, "{}", String::from_utf8_lossy(&v.1.value()))?,
+                (_, _) => for &byte in v.1.value() {
+                    write!(f, "{:x}", byte)?;
+                },
+            }
+        }
         Ok(())
     }
 }
