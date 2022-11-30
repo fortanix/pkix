d41069d658a79111d28e22b74c0f38b48a52ff3f
Pieter Agten <pieter.agten@fortanix.com>
2022-11-30 11:40:36 +0100

    Set recursion_limit to 256 instead of 128

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![recursion_limit="128"]
+#![recursion_limit="256"]
 
 pub extern crate yasna;
 pub extern crate num_bigint;
